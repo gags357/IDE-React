0902fe544ee3df4b28f4fb4666fcc9881480c852
Gagan Yadav <ygagan45@gmail.com>
2021-07-17 17:48:57 +0530

    added file picker functionality

--- src/App.js
+++ src/App.js
@@ -1,41 +1,81 @@
-import React, { useState, useEffect } from 'react';
-import Editor from './components/Editor'
+import React, { useState, useEffect } from "react";
+import Editor from "./components/Editor";
 
 function App() {
-  const [code, setCode] = useState('')
-  const [srcDoc, setSrcDoc] = useState('')
+	const [htmlCode, setHTMLCode] = useState("");
+	const [cssCode, setCSSCode] = useState("");
+	const [jsCode, setJSCode] = useState("");
+	const [activeWindow, setActiveWindow] = useState(0);
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setSrcDoc(`
-          ${code}
-      `)
-    }, 250)
+	const [srcDoc, setSrcDoc] = useState("");
 
-    return () => clearTimeout(timeout)
-  }, [code])
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			let code = htmlCode.replace("<html>", `<html><head><style>${cssCode}</style><script>${jsCode}</script></head>`);
+			setSrcDoc(code);
+		}, 250);
 
-  return (
-    <>
-      <div className="top-pane">
-        <Editor 
-          displayName="CODE" 
-          value={code}
-          onChange={setCode}
-        />
-      </div>
-      <div className="bottom-pane">
-        <iframe 
-          srcDoc={srcDoc}
-          title="output"
-          sandbox="allow-scripts"
-          frameBorder="0"
-          width="100%"
-          height="100%"
-        />
-      </div>
-    </>
-  )
+		return () => clearTimeout(timeout);
+	}, [htmlCode, cssCode, jsCode]);
+
+	const handleCodeChange = (code) => {
+		switch (activeWindow) {
+			default:
+				setHTMLCode(code);
+				break;
+			case 0:
+				setHTMLCode(code);
+				break;
+			case 1:
+				setCSSCode(code);
+				break;
+			case 2:
+				setJSCode(code);
+				break;
+		}
+	};
+
+	const files = [
+		{ filename: "index.html", index: 0 },
+		{ filename: "index.css", index: 1 },
+		{ filename: "index.js", index: 2 },
+	];
+
+	return (
+		<>
+			<div className='top-pane'>
+				<div className='file-picker-container'>
+					{files.map((file) => (
+						<div
+							key={file.index.toString()}
+							className='file-picker'
+							style={{ backgroundColor: file.index === activeWindow ? "#ddd" : "#1f2023", color: file.index === activeWindow ? "#333" : "#ddd" }}
+							onClick={() => {
+								setActiveWindow(file.index);
+							}}>
+							{file.filename}
+						</div>
+					))}
+				</div>
+				<Editor
+					displayName={`index.${activeWindow === 0 ? "html" : activeWindow === 1 ? "css" : "js"}`}
+					value={activeWindow === 0 ? htmlCode : activeWindow === 1 ? cssCode : jsCode}
+					onChange={handleCodeChange}
+					mode={activeWindow === 0 ? "htmlmixed" : activeWindow === 1 ? "css" : "javascript"}
+				/>
+			</div>
+			<div className='bottom-pane'>
+				<iframe
+					srcDoc={srcDoc}
+					title='output'
+					sandbox='allow-forms allow-popups allow-scripts allow-same-origin allow-modals'
+					frameBorder='0'
+					width='100%'
+					height='100%'
+				/>
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,37 +1,34 @@
-import React from 'react'
-import 'codemirror/lib/codemirror.css'
-import 'codemirror/theme/material.css'
-import 'codemirror/mode/htmlmixed/htmlmixed'
-import { Controlled as ControlledEditor } from 'react-codemirror2'
+import React from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/htmlmixed/htmlmixed";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+
+import { Controlled as ControlledEditor } from "react-codemirror2";
 
 export default function Editor(props) {
-    const {
-        displayName,
-        value,
-        onChange
-    } = props
+	const { displayName, value, onChange, mode } = props;
 
-    function handleChange(editor,data,value){
-        onChange(value)
-    }
+	function handleChange(editor, data, value) {
+		onChange(value);
+	}
 
-    return (
-        <div className="editor-container">
-            <div className="editor-title">
-                {displayName}
-            </div>
-            <ControlledEditor 
-                onBeforeChange={handleChange}
-                value={value}
-                className="code-mirror-wrapper"
-                options={{
-                    lineWrapping: true,
-                    lint: true,
-                    mode: 'htmlmixed',
-                    theme: 'material',
-                    lineNumbers: true
-                }}
-            />
-        </div>
-    )
+	return (
+		<div className='editor-container'>
+			<div className='editor-title'>{displayName}</div>
+			<ControlledEditor
+				onBeforeChange={handleChange}
+				value={value}
+				className='code-mirror-wrapper'
+				options={{
+					lineWrapping: true,
+					lint: true,
+					mode,
+					theme: "material",
+					lineNumbers: true,
+				}}
+			/>
+		</div>
+	);
 }
